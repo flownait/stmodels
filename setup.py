0dfc1b48117fed445f3bcd85654c79b7e4112515
Zheng Steven Zhang <zhengst@Zhengs-MacBook-Pro.local>
2019-01-03 14:13:48 +0800

    reupload all scripts

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="stmodels",
     #packages = ['stmodels'],
-    version="0.0.1",
+    version="0.0.0.1",
     author="Steven Zheng",
     author_email="tianwolf@msn.com",
     description="Run Outliers Detection, etc.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/flownait/stmodels",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
